03bd03cc39ac35f47d2abf08dd6bb526b4ad6143
Rishabh Jain <mailrishabh1403@gmail.com>
2020-02-03 14:58:52 +0530

    add netflix

--- content.js
+++ content.js
@@ -1,7 +1,19 @@
 
-
-var msg = `
+const date = new Date();
+if (date.getDay() === 0 || date.getDay === 6) {
+  if (date.getDay() === 6 && date.getHours() >= 20) {
+    console.log("allowing")
+  } else {
+    var msg = `
     <h1>Nope!!!</h1>
 `;
 
-document.write(msg)
+    document.write(msg)
+  }
+} else {
+  var msg = `
+    <h1>Nope!!!</h1>
+`;
+
+  document.write(msg)
+}
